7466a85568d3a45ea0f8c7264bd793acdd4e545c
Fabian Wagner <fabianwagner.fwag@gmail.com>
2026-03-23 16:09:45 +0100

    add js to edit the aria-selected pattern

--- src/js/script.js
+++ src/js/script.js
@@ -15,16 +15,22 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+
+  if (n > slides.length) {slideIndex = 1; }
+  if (n < 1) {slideIndex = slides.length; }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    const isActive = i === (slideIndex -1);
+    dots[i].setAttribute("aria-selected", isActive ? "true": "false");
+
+    dots[i].classList.toggle("active", isActive);
   }
+
   slides[slideIndex-1].style.display = "flex";
-  dots[slideIndex-1].className += " active";
 }
 
 document.getElementById("year").textContent = new Date().getFullYear();
